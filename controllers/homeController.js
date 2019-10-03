6575d6ad40a22433cef4b81bfec9be5903c9143a
Kyle Rhodes <rhodesman93@gmail.com>
2019-10-03 16:41:14 -0700

    refactoring add home and list controllers

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports = function(req, res){
-
-
-    if(req.mysession.seenyou === true){
-      res.writeHead(301, {Location: 'http://localhost:3001/listPageHome'}); //go to main page if logged in
-      res.end();
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + "/../views/home.html").pipe(res);
-
+module.exports = (req, res) => {
+  if (req.mysession.seenyou === true) {
+    res.writeHead(301, { Location: 'http://localhost:3001/listPageHome' });
+    res.end();
+  }
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(path.join(__dirname, '/../views/home.html')).pipe(res);
 };
--- controllers/listHomeController.js
+++ controllers/listHomeController.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports = function(req, res){
-
-
-    if(req.mysession.seenyou === true){
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      fs.createReadStream(__dirname + "/../views/ourList.html").pipe(res);
-    }
-    else {
-      res.writeHead(301, {Location: 'http://localhost:3001/'});
-      res.end(JSON.stringify({errors: "Must be logged in to see list"}));
-    }
-
+module.exports = (req, res) => {
+  if (req.mysession.seenyou === true) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(path.join(__dirname, '/../views/ourList.html')).pipe(res);
+  } else {
+    res.writeHead(301, { Location: 'http://localhost:3001/' });
+    res.end(JSON.stringify({ errors: 'Must be logged in to see list' }));
+  }
 };
